7d3cf269055884e3126312bc12dcb4013acbb009
Chris Anderson <jchris@apache.org>
2010-08-20 15:32:08 -0700

    save tweets to couch

--- _attachments/node/timeline.js
+++ _attachments/node/timeline.js
@@ -8,24 +8,32 @@ var tweetstream = require('tweetstream'),
 var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json')));
 
 
-var stream = tweetstream.createTweetStream({track:config.track, 
-                                            username:config.username, 
+var stream = tweetstream.createTweetStream({track:config.track,
+                                            username:config.username,
                                             password:config.password});
 
+// automatically friend anyone who mentions the term...
 function addFriend(stream, user) {
-  // automatically friend anyone who mentions the term...
-  stream.rest.friendships.create({id:tweet.user.screen_name, user_id:tweet.user.id, follow:true}, 
+  stream.rest.friendships.create({id:tweet.user.screen_name, user_id:tweet.user.id, follow:true},
     function (error, resp, body) {
       if (body) sys.puts(body);
   });
 };
 
 stream.addListener("tweet", function (tweet) {
-
   tweet._id = tweet.id.toString();
-  request({uri:couch, method:'POST', headers:{'content-type':'application/json'}, body:JSON.stringify(tweet)}, 
-    function (e, resp, body) {
+  if (tweet.text && tweet.user) {
+    sys.puts(sys.inspect({
+      text : tweet.text,
+      name : tweet.user.name
+    }));
+  };
+  request({
+    uri:config.couch, method:'POST',
+    headers:{'content-type':'application/json'},
+    body:JSON.stringify(tweet)
+    }, function (e, resp, body) {
       if (resp && resp.statusCode !== 201) sys.puts(sys.inspect(resp), sys.puts(body));
       else {sys.puts(body)};
-  })
+  });
 });
