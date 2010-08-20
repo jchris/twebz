dfd1cc9110e83c5a7e14263473e5010ebbdcd017
Chris Anderson <jchris@apache.org>
2010-08-20 16:02:25 -0700

    use linkup for better links

--- evently/items/_init/selectors/ul/_changes/data.js
+++ evently/items/_init/selectors/ul/_changes/data.js
@@ -1,9 +1,11 @@
 function(r) {
-  var v = r.value, user = v.user;
+  var v = r.value, user = v.user,
+    app = $$(this).app,
+    linkup = app.require("vendor/couchapp/lib/linkup");
   return {
     image_url : user.profile_image_url,
     name : user.screen_name,
     nickname : user.name,
-    message : v.text
+    message : linkup.encode(v.text)
   };
 };
