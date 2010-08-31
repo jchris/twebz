c6e516b7f0bd5ec699eec2863a97595f3bd79c56
Chris Anderson <jchris@apache.org>
2010-08-30 20:49:33 -0700

    list the twitter accounts

--- evently/profile/profileReady/selectors/div.tweet/user_home/data.js
+++ evently/profile/profileReady/selectors/div.tweet/user_home/data.js
@@ -1,7 +1,12 @@
-function() {
-  var data = {};
+function(resp) {
+  var data = {
+    twitter_accounts : []
+  };
   if ($$("#account").userCtx.roles.indexOf("_admin") != -1) {
     data.admin = true;
   }
+  for (var i=0; i < resp.rows.length; i++) {
+    data.twitter_accounts.push({name:resp.rows[i].key});
+  };
   return data;
 };
