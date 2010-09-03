e2b1e0bb33d63dcd24e5b14fd6a705f9cfff2e7a
Chris Anderson <jchris@apache.org>
2010-09-03 07:55:24 -0700

    word cloud autoupdates on changes

--- evently/wordcloud/user/data.js
+++ evently/wordcloud/user/data.js
@@ -1,16 +1,5 @@
-function(userWords, e, params) {
-  var w, cloud = [];
-  for (var i=0; i < userWords.length; i++) {
-    w = userWords[i];
-    if (w.weight >= 2.718) {
-      cloud.push({
-        word : w.word,
-        size : (Math.log(w.weight)) * 10
-      });
-    }
-  };
+function(e, params) {
   return {
-    name : params.screen_name,
-    cloud : cloud
+    name : params.screen_name
   };
 };
