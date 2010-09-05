cb78632f8ed1b9415b167dbd172d0f8f1193e01f
Chris Anderson <jchris@apache.org>
2010-09-05 10:27:38 -0700

    link to archive all tweets

---
+++ evently/items/user/selectors/a[href=#all]/click.js
@@ -0,0 +1,19 @@
+function(e) {
+  var name = e.data.args[1].screen_name.toLowerCase()
+    , a = $(this)
+    , app = $$(a).app
+    ;
+  if ($(a).text() != "Loading...") {
+    app.db.saveDoc({
+      twebz : {
+        type : "user-archive",
+        state : "request",
+        screen_name : name,
+        couch_user : $$("#account").userCtx.name,
+        twitter_acct : $.cookie("twitter_acct")
+      }
+    });
+  }
+  a.text("Loading...");
+  return false;
+};
